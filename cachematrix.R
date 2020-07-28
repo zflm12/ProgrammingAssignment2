cc364224f284dab758a6debd8bc2888309df589a
zmcclinton <zmcclin1@jhu.edu>
2020-07-28 18:12:47 -0400

    1st commit with cache matrix and its inverse

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,33 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## sets the matrix value and the inverse value 
+## gets the values of the matrix and inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  I <- NULL
+  set <- function(y) {
+    x <<- y
+    I <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) I <<- inverse
+  getinverse <- function() I
+  list(set=set, get=get,setinverse=setinverse, getinverse= getinverse)
 }
 
 
 ## Write a short comment describing this function
-
+## checks cache for inverse of given matrix 
+## if not there then will calculate the inverse
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  I <- x$getinverse()
+  if(!is.null(I)) {
+    message("getting cached data")
+    return(I)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
